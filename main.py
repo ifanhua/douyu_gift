5f4d5d1122d145439c9e217ed9202e7539a5c2ea
繁华 <83608916+ifanhua@users.noreply.github.com>
2023-04-11 12:49:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,23 +23,23 @@ def run():
                 logger.info("当前选择模式为:自选模式")
                 nums = conf.get_conf_list('selfMode', 'giftCount')
                 room_list = conf.get_conf_list('selfMode', 'roomId')
-                logger.info("------开始捐赠荧光棒------")
+                logger.info("------开始捐赠钻粉荧光棒------")
                 for i in range(len(nums)):
                     glow_donate(nums[i], room_list[i])
-                logger.info("------荧光棒捐赠结束------")
+                logger.info("------钻粉荧光棒捐赠结束------")
                 get_need_exp()
             elif mode == 0:
                 logger.info("当前选择模式为:平均分配模式")
                 room_list = get_room_list()
                 every_give = math.ceil(glow_nums / len(room_list))
                 left = int(glow_nums) - int(every_give) * (len(room_list) - 1)
-                logger.info("------开始捐赠荧光棒------")
+                logger.info("------开始捐赠钻粉荧光棒------")
                 for room in room_list:
                     if room == room_list[-1]:
                         glow_donate(left, room)
                     else:
                         glow_donate(every_give, room)
-                logger.info("------荧光棒捐赠结束------")
+                logger.info("------钻粉荧光棒捐赠结束------")
                 get_need_exp()
             else:
                 logger.warning("配置错误,没有这种选项,请修改配置并重新执行")
